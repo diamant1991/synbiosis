e4cb90a8c7ba1904feb220aff58209d53dc6fe77
diamant1991 <diamant1988@mail.ru>
2015-03-31 18:02:38 +0300

    other pages

---
+++ js/dropdown.js
@@ -0,0 +1,28 @@
+// Select
+$('.slct').click(function(){
+	var dropBlock = $(this).parent().find('.drop');
+
+	if( dropBlock.is(':hidden') ) {
+		dropBlock.slideDown(250);
+
+		$(this).addClass('active');
+
+		$('.drop').find('li').click(function(){
+
+
+			var selectResult = $(this).html();
+
+			$(this).parent().parent().find('input').val(selectResult);
+
+			$(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+			dropBlock.slideUp(250);
+		});
+
+	} else {
+		$(this).removeClass('active');
+		dropBlock.slideUp(250);
+	}
+
+	return false;
+});
